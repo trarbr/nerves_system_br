da5f7af059529028fba6a175ff7fd23132b1e4ea
Justin Schneck <jschneck@mac.com>
2018-06-27 15:12:41 -0400

    Bump erlinit to 65cfb0c0b7f7dec5d9acf0d210c53e7c7e45acaa

--- package/erlinit/erlinit.mk
+++ package/erlinit/erlinit.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-ERLINIT_VERSION = v1.4.1
+ERLINIT_VERSION = 65cfb0c0b7f7dec5d9acf0d210c53e7c7e45acaa
 ERLINIT_SITE = $(call github,nerves-project,erlinit,$(ERLINIT_VERSION))
 ERLINIT_LICENSE = MIT
 ERLINIT_LICENSE_FILES = LICENSE
